477596a37d845b2046609ef71999532bcb3a2442
Maximilian Alfano-Smith <maxalfanosmith@gmail.com>
2020-06-30 22:00:50 +0000

    Added html and server checks for bad form submissions.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -31,9 +31,12 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
-    String text = getParameter(request, "question-input", "");
+    String text = getParameter(request, "question-input");
 
-    questions.add(text);
+    if (text != null)
+    {
+        questions.add(text);
+    }
 
     // Respond with the result.
     response.sendRedirect("index.html");
@@ -51,11 +54,8 @@ public class DataServlet extends HttpServlet {
    * @return the request parameter, or the default value if the parameter
    *         was not specified by the client
    */
-  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+  private String getParameter(HttpServletRequest request, String name) {
     String value = request.getParameter(name);
-    if (value == null) {
-      return defaultValue;
-    }
     return value;
   }
 }
